ce60172fbaf73df2a92df8d047226244cdb8cab1
jacobo <wdjacobo@gmail.com>
2024-08-05 20:12:52 +0200

    Do exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let reversedString = "";
+    let substraction = 1;
+
+    for (let index = 0; index < string.length; index++) {
+        reversedString += string[string.length-substraction];
+        substraction++;
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
