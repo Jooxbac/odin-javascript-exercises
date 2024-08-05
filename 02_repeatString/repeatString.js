eda04ea719859ac7ca9262785f2f7b865fe582d8
jacobo <wdjacobo@gmail.com>
2024-08-05 20:03:29 +0200

    Do exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let repeatedString = "";
+  if(num>=0){
+    for (let index = 0; index < num; index++) {
+        repeatedString += string;
+      }
+      return repeatedString;
+  }else{
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
