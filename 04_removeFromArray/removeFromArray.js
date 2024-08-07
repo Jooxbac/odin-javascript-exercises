8d0abb4324dd9cb7ad8d9b1fa023e2723936f46c
jacobo <wdjacobo@gmail.com>
2024-08-07 21:02:42 +0200

    Do exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function (
+  array,
+  itemToRemove,
+  ...additionalItemsToRemove
+) {
+  let newArray = [];
+
+  let args = Array.from(arguments).splice(1);
+  console.log("args:" + args);
+
+  for (let i = 0; i < array.length; i++) {
+    let remove = false;
+    for (let j = 0; j < args.length; j++) {
+      if (array[i] === args[j]) {
+        remove = true;
+      }
+    }
+    if (!remove) {
+      newArray.push(array[i]);
+    }
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
