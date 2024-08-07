d3cc5d4e37dd4ab23a7b13238fc550896e2d22ad
jacobo <wdjacobo@gmail.com>
2024-08-07 21:15:10 +0200

    Do exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+  let sum = 0;
 
+  //some param is negative, not an integer or not a number
+  if (
+    firstNumber < 0 ||
+    secondNumber < 0 ||
+    !Number.isInteger(firstNumber) ||
+    !Number.isInteger(secondNumber) ||
+    typeof firstNumber != "number" ||
+    typeof secondNumber != "number"
+  ) {
+    return "ERROR";
+  }
+
+  //allows for loop to work properly with the range
+  if (firstNumber <= secondNumber) {
+    startNumber = firstNumber;
+    endNumber = secondNumber;
+  } else {
+    startNumber = secondNumber;
+    endNumber = firstNumber;
+  }
+
+  for (let index = startNumber; index < endNumber + 1; index++) {
+    sum += index;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
